5982a328fa3072c3984669bf1a0201ada8950bef
Rodrigo Mognon <rodrigo.mognonn@gmail.com>
2024-08-21 22:46:39 -0300

    Implementando consulta de cep da api Viacep e prenchimento automatico dos campos de endereço.

--- 3652-react-forms-projeto_base/src/pages/Cadastro/CadastroEndereco.tsx
+++ 3652-react-forms-projeto_base/src/pages/Cadastro/CadastroEndereco.tsx
@@ -1,5 +1,7 @@
+import { useForm } from "react-hook-form";
 import {
   Button,
+  ErrorMessage,
   Fieldset,
   Form,
   FormContainer,
@@ -8,28 +10,105 @@ import {
   Titulo,
 } from "../../components";
 
+interface FormInputEndereco {
+  cep: string;
+  rua: string;
+  numero: string;
+  bairro: string;
+  localidade: string;
+}
+
 const CadastroEndereco = () => {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    setValue,
+    watch,
+    formState: { errors },
+  } = useForm<FormInputEndereco>();
+
+  const aoSubmeter = (dados: FormInputEndereco) => {
+    console.log(dados);
+  };
+
+  const cepDigitado = watch("cep");
+
+  const fetchEndereco = async (cep: string) => {
+    if (!cep) {
+      setError("cep", { type: "manual", message: "Cep invalido" });
+      return;
+    }
+    try {
+      const reponse = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+      const data = await reponse.json();
+
+      if (reponse.ok) {
+        setValue("rua", data.logradouro);
+        setValue("localidade", `${data.localidade}, ${data.uf}`);
+        setValue("bairro", data.bairro);
+      } else {
+        throw new Error("Cep inválido");
+      }
+      console.log(data);
+    } catch (erro) {
+      console.error(erro);
+    }
+  };
   return (
     <>
       <Titulo>Agora, mais alguns dados sobre você:</Titulo>
-      <Form>
+      <Form onSubmit={handleSubmit(aoSubmeter)}>
         <Fieldset>
           <Label htmlFor="campo-cep">CEP</Label>
-          <Input id="campo-cep" placeholder="Insira seu CEP" type="text" />
+          <Input
+            id="campo-cep"
+            placeholder="Insira seu CEP"
+            type="text"
+            {...register("cep")}
+            $error={!!errors.cep}
+            onBlur={() => fetchEndereco(cepDigitado)}
+          />
+          {errors.cep && <ErrorMessage>{errors.cep.message}</ErrorMessage>}
         </Fieldset>
         <Fieldset>
           <Label htmlFor="campo-rua">Rua</Label>
-          <Input id="campo-rua" placeholder="Rua Agarikov" type="text" />
+          <Input
+            id="campo-rua"
+            placeholder="Rua Agarikov"
+            type="text"
+            {...register("rua")}
+            $error={!!errors.rua}
+          />
+          {errors.rua && <ErrorMessage>{errors.rua.message}</ErrorMessage>}
         </Fieldset>
 
         <FormContainer>
           <Fieldset>
             <Label htmlFor="campo-numero-rua">Número</Label>
-            <Input id="campo-numero-rua" placeholder="Ex: 1440" type="text" />
+            <Input
+              id="campo-numero-rua"
+              placeholder="Ex: 1440"
+              type="text"
+              {...register("numero")}
+              $error={!!errors.numero}
+            />
+            {errors.numero && (
+              <ErrorMessage>{errors.numero.message}</ErrorMessage>
+            )}
           </Fieldset>
           <Fieldset>
             <Label htmlFor="campo-bairro">Bairro</Label>
-            <Input id="campo-bairro" placeholder="Vila Mariana" type="text" />
+            <Input
+              id="campo-bairro"
+              placeholder="Vila Mariana"
+              type="text"
+              {...register("bairro")}
+              $error={!!errors.bairro}
+            />
+            {errors.bairro && (
+              <ErrorMessage>{errors.bairro.message}</ErrorMessage>
+            )}
           </Fieldset>
         </FormContainer>
         <Fieldset>
@@ -38,7 +117,12 @@ const CadastroEndereco = () => {
             id="campo-localidade"
             placeholder="São Paulo, SP"
             type="text"
+            {...register("localidade")}
+            $error={!!errors.localidade}
           />
+          {errors.localidade && (
+            <ErrorMessage>{errors.localidade.message}</ErrorMessage>
+          )}
         </Fieldset>
         <Button type="submit">Cadastrar</Button>
       </Form>
--- 3652-react-forms-projeto_base/src/pages/Cadastro/index.tsx
+++ 3652-react-forms-projeto_base/src/pages/Cadastro/index.tsx
@@ -1,10 +1,12 @@
 import CadastroPessoal from "./CadastroPessoal";
 import { Logotipo } from "../../components";
+import CadastroEndereco from "./CadastroEndereco";
 export default function Cadastro() {
   return (
     <>
       <Logotipo />
-      <CadastroPessoal />
+      {/* <CadastroPessoal /> */}
+      <CadastroEndereco />
     </>
   );
 }
